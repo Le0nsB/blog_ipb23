3e4cb914de803cbeba4c0816d60626a6562e9785
ipb23.l.bluks <ipb23.l.bluks@pjvt.local>
2024-12-11 11:27:58 +0200

    Atceries izdzest to extra mapi

--- Database.php
+++ Database.php
@@ -1,11 +1,17 @@
 <?php
 
 class Database{
-    public function query(){
+    public function query($sql){
         // Data Source Name
         $dsn = "mysql:host=localhost;port=3306;user=root;password=;dbname=blog_ipb23;charset=utf8mb4";
-
         // PDO - PHP Data Object
         $pdo = new PDO($dsn);
+        // 1. Sagatavot vaicājumu (statement)
+        $statement = $pdo->prepare("SELECT * FROM posts");
+        // 2. Izpildīt statement
+        $statement->execute();
+
+        $data =  $statement->fetchAll(PDO::FETCH_ASSOC); 
+        return $data;
     }
 };
--- index.php
+++ index.php
@@ -26,23 +26,8 @@ echo "
 // db nosaukums, parole, lietotājvārds
 // mysql_connect ❌
 
-// Data Source Name
-$dsn = "mysql:host=localhost;port=3306;user=root;password=;dbname=blog_ipb23;charset=utf8mb4";
-
-// PDO - PHP Data Object
-$pdo = new PDO($dsn);
-
 $db = new Database();
-$db->query();
-
-// 1. Sagatavot vaicājumu (statement)
-$statement = $pdo->prepare("SELECT * FROM posts");
-
-// 2. Izpildīt statement
-$statement->execute();
-
-// 3. Dabūt rezultātu
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query("SELECT * FROM posts");
 
 //dd($posts[0]["content"]);
 
