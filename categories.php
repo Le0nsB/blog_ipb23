2b7b46e8097f0e5074efd45f4616e0f1282e76f1
ipb23.l.bluks <ipb23.l.bluks@pjvt.local>
2025-01-07 14:58:50 +0200

    Kategorijas

---
+++ categories.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Logs</title>
+  <link rel="stylesheet" href="categories_style.css">
+</head>
+<body>
+</body>
+</html>
+<?php
+
+require "functions.php";
+require "Database.php";
+
+$config = require("config.php");
+
+$db = new Database($config["database"]);
+
+$select = "SELECT * FROM categories";
+$params = [];
+if (isset($_GET["search_query"]) && $_GET["search_query"] != "" ){
+  $search_query = "%" . $_GET["search_query"] . "%";
+  $select .= " WHERE category_name LIKE :nosaukums";
+  $params = ["nosaukums" => $search_query];
+}
+$categories = $db->query($select, $params)->fetchAll();
+
+echo "<h1>Logs</h1>";
+echo "<form>";
+  echo "<input name='search_query'/>";
+  echo "<button>🕵️‍♂️</button>";
+echo "</form>";
+
+if(count($categories) == 0){
+  echo $_GET["search_query"] . "  nav atrasts";
+};
+// Ar foreach izvadīt content
+echo "<div>";
+echo "<ul>";
+foreach($categories as $category){
+  echo "<li>" . $category["category_name"] . "</li>";
+}
+echo "</ul>";
+echo "</div>";
