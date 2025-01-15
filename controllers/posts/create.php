34b3a0000ace25a62163a4449faed01c5ae93b61
Leons <114695895+IlusionLion@users.noreply.github.com>
2025-01-15 21:17:27 +0200

    10 izpildits

--- controllers/posts/create.php
+++ controllers/posts/create.php
@@ -1,15 +1,23 @@
 <?php
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $sql = "INSERT INTO posts 
-    (content)
-    VALUES
-    (:content)";
-    $params = ["content" => $_POST["content"]];
-    $post = $db->query($sql, $params)->fetch();
-    header("Location: /"); exit();
-}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $errors = [];
 
+    if (!isset($_POST["content"]) || strlen($_POST["content"]) == 0) {
+        $errors["content"] = "Saturam jābūt ievadītam.";
+    } elseif (strlen($_POST["content"]) > 50) {
+        $errors["content"] = "Saturam jābūt ne garākam par 50 rakstzīmēm.";
+    }
+
+    if (empty($errors)) {
+        $sql = "INSERT INTO posts (content) VALUES (:content)";
+        $params = ["content" => $_POST["content"]];
+        $db->query($sql, $params);
+        header("Location: /");
+        exit();
+    }
+}
 
 $pageTitle = "Create";
 $style = "css/create.style.css";
--- views/posts/create.view.php
+++ views/posts/create.view.php
@@ -1,9 +1,14 @@
-<?php require "views/components/header.php"?>
-<?php require "views/components/navbar.php"?>
+<?php require "views/components/header.php" ?>
+<?php require "views/components/navbar.php" ?>
 
 <h1>Izveidot bloga ierakstu</h1>
 <form method="POST">
-    <label><input name="content" /></label>
+    <label>
+        <input name="content" value="<?= htmlspecialchars($_POST['content'] ?? '') ?>" />
+    </label>
+    <?php if(isset($errors["content"])) { ?>
+       <p><?= htmlspecialchars($errors["content"]) ?></p>
+     <?php } ?>
 </form>
 
 <?php require "views/components/footer.php" ?>
