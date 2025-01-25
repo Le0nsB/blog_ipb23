f10553d1574b4775fe80df1c412877883e7075fa
Leons <114695895+IlusionLion@users.noreply.github.com>
2025-01-25 13:20:44 +0200

    @keyframes un CSS saīsinašana

--- views/posts/show.view.php
+++ views/posts/show.view.php
@@ -2,9 +2,9 @@
 <?php require "views/components/navbar.php"?>
 
 <h1><?= htmlspecialchars($post["content"]) ?></h1>
-<a href="edit?id=<?= $post["id"] ?>" class="edit"><p>Edit</p></a> 
+<a href="edit?id=<?= $post["id"] ?>" class="edit"><p>Edit </p><img src="pencil.png"></a> 
 <form action="/delete" method="POST">
     <input name="id" value = "<?= $post["id"] ?>" type="hidden"/>
-    <button>Dzest</button>
+    <button>Dzest<img src="trash.png"></button>
 </form>
 <?php require "views/components/footer.php" ?>
