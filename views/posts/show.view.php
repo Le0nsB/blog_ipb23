d97be11cdc9a0599ef7fa87a39f5f95817143b46
Leons <114695895+IlusionLion@users.noreply.github.com>
2025-02-19 18:11:27 +0200

    Vel biki CSS

--- views/posts/show.view.php
+++ views/posts/show.view.php
@@ -4,23 +4,28 @@
 <h1><?= htmlspecialchars($post["content"]) ?></h1>
 <h2><?= $post["category_name"] ?><h2>
 <div class="cont">
-    <div>
-        <a href="edit?id=<?= $post["id"] ?>" class="edit"><p>Edit </p><img src="pencil.png"></a> 
 
-        <form action="/delete" method="POST">
+    <div class="show_buttons">
+        <a href="edit?id=<?= $post["id"] ?>" class="show_edit"><p>Edit </p><img src="pencil.png"></a> 
+
+        <form action="/delete" method="POST" >
             <input name="id" value = "<?= $post["id"] ?>" type="hidden" class="input"/>
-            <button class="delete">Dzest<img src="trash.png" ></button>
+            <button class="show_delete">Dzest<img src="trash.png" ></button>
         </form>
+    </div>
+
+    <div class="cont_2">
+        <input class="input">Autors:</input>
+        <input class="input">Komentars:</input>
+        <button action="/comments/create" class="submit">Submit<img src="send.png" ></button>
     </div>    
-        <div>
-            <p>Kip komentars</p>
+
+    <div class="cont_2">
+        <p>Kip komentars</p>
+        <div class="comment_buttons">
             <a action="/comments/edit" class="edit">Edit<img src="pencil.png"></a>
             <button action="/comments/delete" class="delete">Dzest<img src="trash.png" ></button>
         </div>
-        <div>
-            <input class="input">Autors:</input>
-            <input class="input">Komentars:</input>
-            <button action="/comments/create" class="submit">Submit<img src="send.png" ></button>
-        </div>
+    </div>
 </div> 
 <?php require "views/components/footer.php" ?>
