31e2140e09cef8292d8381fcf79545fe9dfb64b3
ipb23.l.bluks <ipb23.l.bluks@pjvt.local>
2025-02-19 13:45:30 +0200

    CSSSSSSSSSSSSSSSSSSS

--- views/posts/show.view.php
+++ views/posts/show.view.php
@@ -3,26 +3,24 @@
 
 <h1><?= htmlspecialchars($post["content"]) ?></h1>
 <h2><?= $post["category_name"] ?><h2>
-
-<a href="edit?id=<?= $post["id"] ?>" class="edit"><p>Edit </p><img src="pencil.png"></a> 
-
-<form action="/delete" method="POST">
-    <input name="id" value = "<?= $post["id"] ?>" type="hidden"/>
-    <button class="delete">Dzest<img src="trash.png" ></button>
-</form>
-<div>
-    <h3>Komentari</h3>
-
+<div class="cont">
     <div>
-        <p>Kip komentars</p>
-        <button action="/comments/edit">Edit</button>
-        <button action="/comments/delete" class="delete">Delete</button>
-    </div>
+        <a href="edit?id=<?= $post["id"] ?>" class="edit"><p>Edit </p><img src="pencil.png"></a> 
 
-    <div>
-        <input>:Autors</input>
-        <input>:Komentars</input>
-        <button action="/comments/create">Submit</button>
-    </div>
-</div>
+        <form action="/delete" method="POST">
+            <input name="id" value = "<?= $post["id"] ?>" type="hidden" class="input"/>
+            <button class="delete">Dzest<img src="trash.png" ></button>
+        </form>
+    </div>    
+        <div>
+            <p>Kip komentars</p>
+            <a action="/comments/edit" class="edit">Edit<img src="pencil.png"></a>
+            <button action="/comments/delete" class="delete">Dzest<img src="trash.png" ></button>
+        </div>
+        <div>
+            <input class="input">Autors:</input>
+            <input class="input">Komentars:</input>
+            <button action="/comments/create" class="submit">Submit<img src="send.png" ></button>
+        </div>
+</div> 
 <?php require "views/components/footer.php" ?>
